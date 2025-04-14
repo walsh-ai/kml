cd5701946422437abaf0e8df17f6c852a619cd38
walsh-ai <walsh.jthomas@outlook.com>
2025-04-14 15:53:39 +0100

    Every postcode is present in db

--- verify-db.py
+++ verify-db.py
@@ -1,5 +1,6 @@
 import sqlite3 as sql
 import json
+import geopandas as gpd
 
 def get_feature_by_postcode(db_path="postcodes_multi.db", postcode_name="CM21"):
     postcode_name = postcode_name.strip().upper()
@@ -11,9 +12,29 @@ def get_feature_by_postcode(db_path="postcodes_multi.db", postcode_name="CM21"):
             cursor.execute(sql_query, (postcode_name,))
             return cursor.fetchone()
     except sql.OperationalError as e:
-        print("Failed to open database:", e)
+        print("Failed to open database: ", e)
 
     return None
+
+
+def verify_every_postcode(db_path="postcodes_multi.db", geojson_path="PostcodeDistrictsPolygons_multi.geojson"):
+    geojsonFile = open(geojson_path)
+    gdf = gpd.read_file(geojsonFile)
+    sql_query = """SELECT data FROM postcodeDistrictGeoJSON WHERE name = ?"""
+
+    try:
+        with sql.connect(db_path) as conn:
+            cursor = conn.cursor()
+            for idx, row in gdf.iterrows():
+                name = row.get("name")
+                cursor = conn.cursor()
+                cursor.execute(sql_query, (name,))
+                if not cursor.fetchone():
+                    return False
+    except sql.OperationalError as e:
+        print("Failed to open database: ", e)
+    
+    return True
 
 
 selectedRow = get_feature_by_postcode()
@@ -22,3 +43,8 @@ if selectedRow:
     print(json.dumps(geojson, indent=4))
 else:
     print("No data returned.")
+
+if verify_every_postcode():
+    print("*** EVERY POSTCODE IS PRESENT ***")
+
+
